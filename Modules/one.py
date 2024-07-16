bde26e3e261319adf0ff4586b8952aa57daf0c1a
Anif-Aj <anifaj1149@gmail.com>
2024-07-16 15:52:37 +0530

    created mini projects and modules and packages

--- Modules/one.py
+++ Modules/one.py
@@ -6,4 +6,4 @@ print("top-level print inside of one.py")
 if __name__ == "__main__":
     print("one.py is being run directly")
 else:
-    print("one.py is being imported into another module") 
+    print("one.py is being imported into another module")
